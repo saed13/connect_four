1ceb6e2faba32ccac3f1ce534b4abac940414a06
saed <sa13291@stud.th-deg.de>
2022-05-01 03:24:09 +0200

    transpose and print the matrix as a string
    - bug fix print in the first element of the array
    - start game using a self function
    - print X or O depending on the player

--- user-interface/GameBoard.py
+++ user-interface/GameBoard.py
@@ -1,43 +1,92 @@
 class GameBoard:
     board = []
     p1 = True
+    p2 = False
     p1symbol = "X"
     p2symbol = "O"
 
     def __init__(self):
-        self.column = None
         self.p = None
-        self.board = [[], [], [], [], [], []]
+        self.board = [[], [], [], [], [], [], []]
+
         for e in self.board:
-            for i in range(0, 7):
+            for i in range(0, 8):
                 e.append(" ")
 
     def __str__(self):
+        transposed = self.transpose(self.board)
+
         currentBoard = ""
-        for e in self.board:
+        for e in transposed:
             for i in e:
-                currentBoard +=  i + " "
+                currentBoard += i + " | "
             currentBoard += "\n"
+
         return currentBoard
 
+    def startGame(self):
+        """
+
+        :return:
+        """
+        while True:
+            x = self.askInput
+            print(x)
+
+    @property
     def askInput(self):
+        """
+
+        :return:
+        """
         self.p = 1 if self.p1 else 2
-        self.column = input(f"Player {self.p}, enter your move[1-7]:")
-        p1 = not self.p1
-        return int(self.column)
+        column = input(f"Player {self.p}, enter your move[1-7]:")
+
+        result = self.addToken(int(column))
+        return result
 
     def addToken(self, column):
+        """
+
+        :param column:
+        :return:
+        """
         currentBoard = self.board
         column = column - 1
-        for i in range(6, 0, -1):
+
+        for i in range(7, -1, -1):
             if currentBoard[column][i] == " ":
-                currentBoard[column][i] = self.p1symbol
+                currentBoard[column][i] = self.p1symbol if self.p1 else self.p2symbol
+
+                self.p1 = not self.p1
+                self.p2 = not self.p2
                 break
+
         self.board = currentBoard
-        return self.board
+        return str(gameBoard)
+
+    def transpose(self, matrix):
+        """
+
+        :param matrix:
+        :return:
+        """
+        rows = len(matrix)
+        columns = len(matrix[0])
+
+        matrix_t = []
+        for j in range(columns):
+            row = []
+            for i in range(rows):
+                row.append(matrix[i][j])
+            matrix_t.append(row)
+
+        return matrix_t
+
 
 gameBoard = GameBoard()
-while True:
-    x = gameBoard.askInput()
-    y = gameBoard.addToken(x)
-    print(y)
+
+gameBoard.startGame()
+
+
+
