b8e8479de86f66acdf1851b27759d0b9739d1642
Saed Abed <saed.abed@stud.th-deg.de>
2022-05-08 21:41:57 +0200

    Update features/steps/throw-a-token-steps.py

--- features/steps/throw-a-token-steps.py
+++ features/steps/throw-a-token-steps.py
@@ -27,7 +27,7 @@ def step_impl(context):
     context.driver.set_window_size(1920, 1080, context.driver.window_handles[0])
     context.action_chains = ActionChains(context.driver)
 
-    context.driver.get("http://docker:5000")
+    context.driver.get("http://localhost:3000")
     time.sleep(1)
 
 
